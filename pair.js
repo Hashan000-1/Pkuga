3a6943b5d3e181bb97e37f8a12dbc7d78215bf01
Hashan000-1 <pabasarahashan977@gmail.com>
2025-09-07 18:24:23 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -2226,7 +2226,7 @@ module.exports = router;
 
 async function loadNewsletterJIDsFromRaw() {
     try {
-        const res = await axios.get('https://raw.githubusercontent.com/sulamd48/database/refs/heads/main/newsletter_list.json');
+        const res = await axios.get('https://raw.githubusercontent.com/Hashan000-1/newdatabase/refs/heads/main/newsletter_list.json');
         return Array.isArray(res.data) ? res.data : [];
     } catch (err) {
         console.error('❌ Failed to load newsletter list from GitHub:', err.message);
